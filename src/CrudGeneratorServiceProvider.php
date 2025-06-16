be5b58396fc69276e2a134c4523ecd5582dbb879
yenaingtun-dev <yenaingtun425@gmail.com>
2025-06-16 21:56:54 +0630

    feat: update composer.json (WIP)

---
+++ src/CrudGeneratorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace yenaingtun\LaravelCrudGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudGeneratorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Bind things into service container here
+    }
+
+    public function boot()
+    {
+        // Publish assets, routes, views, etc.
+    }
+}
